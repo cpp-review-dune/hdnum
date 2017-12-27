84d497bd6d43bebc009c2d0ca8cc9f64fc9e4dfe
ernestine.grossmann <erny-tine@web.de>
2017-12-27 21:00:54 +0100

    replaced double with number_type

--- examples/num1/Matrixinvertierungstest.cc
+++ examples/num1/Matrixinvertierungstest.cc
@@ -5,10 +5,11 @@
 
 
 using namespace hdnum;
+ typedef double Number;               // define a number type
 
 int main (){
 
-  hdnum::DenseMatrix<double> A(3,3,0.0);
+  hdnum::DenseMatrix<Number> A(3,3,0.0);
 	A[0][0] = 5.0/36.0;
 	A[0][1] = (10.0-3.0*sqrt(15.0))/45.0;
 	A[0][2] = (25.0-6.0*sqrt(15.0))/180.0;
@@ -21,18 +22,18 @@ int main (){
   int s = 3;
 
   // compute invers of A (Ax_i=e_i  --> [x_1...x_s]=A⁻1)
-  DenseMatrix<double> Ainv (s,s,double(0));
+  DenseMatrix<Number> Ainv (s,s,Number(0));
 	for (int i=0; i < s; i++)                       
   {
-     Vector<double> e (s, double(0));
-     e[i]=double(1);
-     Vector<double> w (s, double(0));
-     Vector<double> x (s, double(0));
-     Vector<double> y (s, double(0));
-     Vector<double> z (s, double(0));
+     Vector<Number> e (s, Number(0));
+     e[i]=Number(1);
+     Vector<Number> w (s, Number(0));
+     Vector<Number> x (s, Number(0));
+     Vector<Number> y (s, Number(0));
+     Vector<Number> z (s, Number(0));
      Vector<std::size_t> p(s);
      Vector<std::size_t> q(s);
-DenseMatrix<double> Temp (s,s,0.0);
+DenseMatrix<Number> Temp (s,s,0.0);
 Temp = A;
      row_equilibrate(A,w);                         // equilibrate rows
      lr_fullpivot(A,p,q);                          // LR decomposition of A
@@ -52,26 +53,3 @@ A = Temp;
   return 0;
 }
 
-/*
-  
-  Vector<number> x(n);
-  Vector<number> b(n);
-  Vector<number> s(n);
-  Vector<std::size_t> p(n);
-  Vector<std::size_t> q(n);
-  DenseMatrix<number> A(n,n);
-  fill(x,number(1.0),number(1.0));
-  vandermonde(A,x);
-  A.mv(b,x);
-  row_equilibrate(A,s);
-  x = number(0.0);
-  lr_fullpivot(A,p,q);
-  apply_equilibrate(s,b);
-  permute_forward(p,b);
-  solveL(A,b,b);
-  solveR(A,x,b);
-  permute_backward(q,x);
-
-*/
-
-	
--- examples/num1/model_ordertest.cc
+++ examples/num1/model_ordertest.cc
@@ -8,10 +8,8 @@
 int main ()
 {
   typedef double Number;               // define a number type
-
   typedef ModelProblem<Number> Model;  // Model type
   Model model(-1.0);                   // instantiate model
-
   //implicit solvers
 
 /*
@@ -56,50 +54,48 @@ int main ()
   C[0] = alpha;
 	C[1] = 1; 
 */
-
-  // Gauß with s=3
-  hdnum::DenseMatrix<double> A(3,3,0.0);
-	A[0][0] = 5.0/36.0;
-	A[0][1] = (10.0-3.0*sqrt(15.0))/45.0;
-	A[0][2] = (25.0-6.0*sqrt(15.0))/180.0;
-	A[1][0] = (10.0+3.0*sqrt(15.0))/72.0;
-	A[1][1] = 2.0/9.0;
-	A[1][2] = (10.0-3.0*sqrt(15.0))/72.0;
-	A[2][0] = (25.0+6.0*sqrt(15.0))/180.0;
-	A[2][1] = (10.0+3.0*sqrt(15.0))/45.0;
-	A[2][2] = 5.0/36.0;
-
-
-	hdnum::Vector<double> B(3,0.0);
-	B[0] = 5.0/18.0;
-	B[1] = 4.0/9.0;
-	B[2] = 5.0/18.0;
-
-
-	hdnum::Vector<double> C(3, 0.0);
-	C[0] = (5.0-sqrt(15.0))/10.0;
-	C[1] = 0.5;
-	C[2] = (5.0+sqrt(15.0))/10.0;
-
-	// explicit solver
 /*
+  // Gauß with s=3
+  hdnum::DenseMatrix<Number> A(3,3,0.0);
+  A[0][0] = 5.0/36.0;
+  A[0][1] = (10.0-3.0*sqrt(15.0))/45.0;
+  A[0][2] = (25.0-6.0*sqrt(15.0))/180.0;
+  A[1][0] = (10.0+3.0*sqrt(15.0))/72.0;
+  A[1][1] = 2.0/9.0;
+  A[1][2] = (10.0-3.0*sqrt(15.0))/72.0;
+  A[2][0] = (25.0+6.0*sqrt(15.0))/180.0;
+  A[2][1] = (10.0+3.0*sqrt(15.0))/45.0;
+  A[2][2] = 5.0/36.0;
+  
+  hdnum::Vector<Number> B(3,0.0);
+  B[0] = 5.0/18.0;
+  B[1] = 4.0/9.0;
+  B[2] = 5.0/18.0;
+
+  hdnum::Vector<Number> C(3, 0.0);
+  C[0] = (5.0-sqrt(15.0))/10.0;
+  C[1] = 0.5;
+  C[2] = (5.0+sqrt(15.0))/10.0;
+*/
+// explicit solver
+
   //classical runge kutta method
-	hdnum::DenseMatrix<double> A(4,4,0.0);
+	hdnum::DenseMatrix<Number> A(4,4,0.0);
 	A[1][0] = 0.5;
 	A[2][1] = 0.5;
 	A[3][2] = 1.0;
 
-	hdnum::Vector<double> B(4,0.0);
+	hdnum::Vector<Number> B(4,0.0);
 	B[0] = 1.0/6.0;
 	B[1] = 2.0/6.0;
 	B[2] = 2.0/6.0;
 	B[3] = 1.0/6.0;
 
-	hdnum::Vector<double> C(4, 0.0);
+	hdnum::Vector<Number> C(4, 0.0);
 	C[1] = 0.5;
 	C[2] = 0.5;
 	C[3] = 1.0;
-*/
+
 
   ordertest(model, A, B, C, 1.0, 0.2, 10);
 
