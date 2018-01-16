2fa2d9c31353457fa8d7e5b7383801ee3078c431
René Heß <rene.hess@iwr.uni-heidelberg.de>
2018-01-16 15:45:33 +0100

    Rename modelproblemkutta and add to build system

--- examples/num1/modelproblem_runge_kutta.cc
+++ examples/num1/modelproblem_runge_kutta.cc
@@ -6,32 +6,32 @@
 
 int main ()
 {
-  typedef double Number;               // define a number type
-
-  typedef ModelProblem<Number> Model;  // Model type
-  Model model(-1.0);                   // instantiate model
+  // Define modelproblem
+  typedef double Number;
+  typedef ModelProblem<Number> Model;
+  Model model(-1.0);
 
   // Gauß with s=3
   hdnum::DenseMatrix<double> A(3,3,0.0);
-	A[0][0] = 5.0/36.0;
-	A[0][1] = (10.0-3.0*sqrt(15.0))/45.0;
-	A[0][2] = (25.0-6.0*sqrt(15.0))/180.0;
-	A[1][0] = (10.0+3.0*sqrt(15.0))/72.0;
-	A[1][1] = 2.0/9.0;
-	A[1][2] = (10.0-3.0*sqrt(15.0))/72.0;
-	A[2][0] = (25.0+6.0*sqrt(15.0))/180.0;
-	A[2][1] = (10.0+3.0*sqrt(15.0))/45.0;
-	A[2][2] = 5.0/36.0;
+  A[0][0] = 5.0/36.0;
+  A[0][1] = (10.0-3.0*sqrt(15.0))/45.0;
+  A[0][2] = (25.0-6.0*sqrt(15.0))/180.0;
+  A[1][0] = (10.0+3.0*sqrt(15.0))/72.0;
+  A[1][1] = 2.0/9.0;
+  A[1][2] = (10.0-3.0*sqrt(15.0))/72.0;
+  A[2][0] = (25.0+6.0*sqrt(15.0))/180.0;
+  A[2][1] = (10.0+3.0*sqrt(15.0))/45.0;
+  A[2][2] = 5.0/36.0;
 
-	hdnum::Vector<double> B(3,0.0);
-	B[0] = 5.0/18.0;
-	B[1] = 4.0/9.0;
-	B[2] = 5.0/18.0;
+  hdnum::Vector<double> B(3,0.0);
+  B[0] = 5.0/18.0;
+  B[1] = 4.0/9.0;
+  B[2] = 5.0/18.0;
 
-	hdnum::Vector<double> C(3, 0.0);
-	C[0] = (5.0-sqrt(15.0))/10.0;
-	C[1] = 0.5;
-	C[2] = (5.0+sqrt(15.0))/10.0;
+  hdnum::Vector<double> C(3, 0.0);
+  C[0] = (5.0-sqrt(15.0))/10.0;
+  C[1] = 0.5;
+  C[2] = (5.0+sqrt(15.0))/10.0;
 
   typedef hdnum::RungeKutta_n<Model> Solver;    // Solver type
   Solver solver(model, A, B, C);                // instantiate solver
