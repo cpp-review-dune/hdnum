64dd7557f2406fe5ed299d6c93db3eef7c987d44
Marian Piatkowski <marian.piatkowski@iwr.uni-heidelberg.de>
2018-04-23 16:29:21 +0200

    fixes on slides for short introduction to programming

--- examples/progkurs/zahlen.cc
+++ examples/progkurs/zahlen.cc
@@ -5,6 +5,6 @@ int main ()
   unsigned int i; // uninitialisierte natuerliche Zahl
   double x(3.14); // initialisierte Fliessommazahl
   float y(1.0);   // einfache Genauigkeit
-  short j(3);     // eine "kleine" Zahl
+  short j(3);     // eine 'kleine' Zahl
   std::cout << "(i+x)*(y+j)=" << (i+x)*(y+j) << std::endl;
 }
