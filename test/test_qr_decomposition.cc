521f7673c61267513bdba798bc253bafcafced06
Raphael Vogt <cx238@stud.uni-heidelberg.de>
2021-02-26 15:57:59 +0100

    qr decomposition now also works with non small matrices

--- test/test_qr_decomposition.cc
+++ test/test_qr_decomposition.cc
@@ -21,48 +21,9 @@
  */
 
 
-// check preconditions
-TEST(TestQRDecomposition, TestPreconditions)
-{
-    // create a square matrix
-    hdnum::DenseMatrix<double> A(10, 10);
-    // check that there's an exception by running qr decomposition
-    EXPECT_THROW({
-        try
-        {
-            hdnum::qr_decomposition_gram_schmidt(A);
-        }
-        catch(const hdnum::ErrorException& e)
-        {
-            // check that the exception message is right
-            std::size_t found = e.what().find("The Matrix is not a small matrix!");
-            ASSERT_TRUE(found != e.what().npos);
-            throw;
-        }
-    }, hdnum::ErrorException);
-
-    // repeat with a matrix n > m
-    hdnum::DenseMatrix<double> B(6, 10);
-    // check that there's an exception by runnig qr decomposition
-    EXPECT_THROW({
-        try
-        {
-            hdnum::qr_decomposition_gram_schmidt(B);
-        }
-        catch(const hdnum::ErrorException& e)
-        {
-            // check that the exception message is right
-            std::size_t found = e.what().find("The Matrix is not a small matrix!");
-            ASSERT_TRUE(found != e.what().npos);
-            throw;
-        }
-    }, hdnum::ErrorException);
-    
-}
-
 
 // check postconditions
-TEST(TestQRDecomposition, TestPostconditions)
+TEST(TestQRDecomposition, TestPostconditionsSmallMatrix)
 {
     // create a random size small matrix
     int m;
@@ -206,44 +167,291 @@ TEST(TestQRDecomposition, TestPostconditions)
     }
 }
 
-// check preconditions
-TEST(TestQRDecompositionPivoting, TestPreconditions)
+// check postconditions
+TEST(TestQRDecomposition, TestPostconditionsSquareMatrix)
 {
-    // create a square matrix
-    hdnum::DenseMatrix<double> A(10, 10);
+    // create a random size small matrix
+    int m;
+    srand(time(NULL));
+    m = (rand() % 18) + 2;    // [2, ... ,20]
+    hdnum::DenseMatrix<double> Q(m, m);
 
-    // check that there's an exception by runnig qr decomposition
-    EXPECT_THROW({
-        try
-        {
-            hdnum::qr_decomposition_gram_schmidt_pivoting(A);
+    // fill it with random elements
+    for (int i=0; i < Q.rowsize(); i++) {
+        for (int j=0; j < Q.colsize(); j++) {
+            int x = (rand() % 200) - 100;    // [-100, ... ,100]
+            Q(i, j) = x;
         }
-        catch(const hdnum::ErrorException& e)
-        {
-            // check that the exception message is right
-            std::size_t found = e.what().find("The Matrix is not a small matrix!");
-            ASSERT_TRUE(found != e.what().npos);
-            throw;
-        }
-    }, hdnum::ErrorException);
+    }
 
-    // repeat with a matrix n > m
-    hdnum::DenseMatrix<double> B(6, 10);
-    // check that there's an exception by runnig qr decomposition
-    EXPECT_THROW({
-        try
-        {
-            hdnum::qr_decomposition_gram_schmidt_pivoting(B);
+    // save it before overwritting
+    hdnum::DenseMatrix<double> A(Q);
+
+    // run qr decomposition and save R
+    hdnum::DenseMatrix<double> R(hdnum::qr_decomposition_gram_schmidt(Q));
+
+    // declare a threshold for the asserts
+    double threshold = 0.00000001;
+
+    // check Q ∈ K^(mxn)
+    ASSERT_EQ(Q.colsize(), A.colsize());
+    ASSERT_EQ(Q.rowsize(), A.rowsize());
+
+    // check that norm(qi) = 1
+    for (int i=0; i < Q.colsize(); i++) {
+        double norm(0.0);
+
+        for (int j=0; j < Q.rowsize(); j++) {
+            norm += Q(j, i) * Q(j, i);
         }
-        catch(const hdnum::ErrorException& e)
-        {
-            // check that the exception message is right
-            std::size_t found = e.what().find("The Matrix is not a small matrix!");
-            ASSERT_TRUE(found != e.what().npos);
-            throw;
+        norm = sqrt(norm);
+        ASSERT_NEAR(norm, 1.0, threshold);
+    }
+
+    // check that Q^T * Q = I
+    hdnum::DenseMatrix<double> I(Q.transpose()*Q);
+    for (int i=0; i < I.rowsize(); i++) {
+        for (int j=0; j < I.colsize(); j++) {
+            // main diagonal
+            if (j == i) {
+                ASSERT_NEAR(I(i, j), 1.0, threshold);
+                continue;
+            }
+            // other elements
+            ASSERT_NEAR(I(i, j), 0.0, threshold);
         }
-    }, hdnum::ErrorException);
-    
+    }
+
+    // R ∈ K^(nxn)
+    ASSERT_EQ(R.rowsize(), A.colsize());
+    ASSERT_EQ(R.colsize(), A.colsize());
+
+    // R is an upper triangular matrix
+    for (int i=0; i < R.colsize(); i++) {
+        for (int j=i+1; j < R.rowsize(); j++) {
+            ASSERT_NEAR(R(j, i), 0.0, threshold);
+        }
+    }
+
+    // A = Q*R
+    hdnum::DenseMatrix<double> QR(Q*R);
+    ASSERT_EQ(QR.rowsize(), A.rowsize());
+    ASSERT_EQ(QR.colsize(), A.colsize());
+    for (int i=0; i < QR.rowsize(); i++) {
+        for (int j=0; j < QR.colsize(); j++) {
+            ASSERT_NEAR(QR(i, j), A(i, j), threshold);
+        }
+    }
+
+    // now repeat with GNU multiple precision types
+    mpf_set_default_prec(2048);
+
+    // declare a new more precise threshold
+    mpf_class threshold_gmp = 0.00000000000000000001;
+
+    // create A, Q and R with gmp datatypes
+    hdnum::DenseMatrix<mpf_class> A_gmp(m, m);
+    for (int r=0; r < A_gmp.rowsize(); r++) {
+        for (int c=0; c < A_gmp.colsize(); c++) {
+            A_gmp(r, c) = A(r, c);
+        }
+    }
+
+    hdnum::DenseMatrix<mpf_class> Q_gmp(A_gmp);
+    hdnum::DenseMatrix<mpf_class> R_gmp(hdnum::qr_decomposition_gram_schmidt(Q_gmp));
+
+    // check that norm(qi) = 1
+    for (int i=0; i < Q_gmp.colsize(); i++) {
+        mpf_class norm = 0;
+        mpf_class expected_norm = 1;
+
+        for (int j=0; j < Q_gmp.rowsize(); j++) {
+            norm += Q_gmp(j, i) * Q_gmp(j, i);
+        }
+        norm = sqrt(norm);
+        ASSERT_TRUE( (norm > expected_norm-threshold_gmp) && (norm < expected_norm+threshold_gmp));     // 1-threshold < norm < 1+threshold
+    }
+
+    // check that Q_gmp^T * Q_gmp = I
+    hdnum::DenseMatrix<mpf_class> I_gmp(Q_gmp.transpose()*Q_gmp);
+    for (int i=0; i < I_gmp.rowsize(); i++) {
+        for (int j=0; j < I_gmp.colsize(); j++) {
+            // main diagonal
+            if (j == i) {
+                ASSERT_TRUE((I_gmp(i, i) > 1-threshold_gmp) && (I_gmp(i, i) < 1+threshold_gmp));         // 1-threshold < I(i, i) < 1+threshold
+                continue;
+            }
+            // other elements
+            ASSERT_TRUE((I_gmp(i, j) > 0-threshold_gmp) && (I_gmp(i, j) < 0+threshold_gmp));             // 0-threshold < I(i, j) < 0+threshold
+        }
+    }
+
+    // R_gmp ∈ K^(nxn)
+    ASSERT_EQ(R_gmp.rowsize(), A_gmp.colsize());
+    ASSERT_EQ(R_gmp.colsize(), A_gmp.colsize());
+
+    // R_gmp is an upper triangular matrix
+    for (int i=0; i < R_gmp.colsize(); i++) {
+        for (int j=i+1; j < R_gmp.rowsize(); j++) {
+            ASSERT_TRUE((R_gmp(j, i) > 0-threshold_gmp) && (R_gmp(j, i) < 0+threshold_gmp));             // 0-threshold < I(i, j) < 0+threshold
+        }
+    }
+
+    // A_gmp = Q_gmp*R_gmp
+    hdnum::DenseMatrix<mpf_class> QR_gmp(Q_gmp*R_gmp);
+    ASSERT_EQ(QR_gmp.rowsize(), A_gmp.rowsize());
+    ASSERT_EQ(QR_gmp.colsize(), A_gmp.colsize());
+    for (int i=0; i < QR_gmp.rowsize(); i++) {
+        for (int j=0; j < QR_gmp.colsize(); j++) {
+            ASSERT_TRUE((QR_gmp(i, j) > A_gmp(i, j)-threshold_gmp) && (QR_gmp(i, j) < A_gmp(i, j)+threshold_gmp));     // A_gmp(i, j)-threshold < QR_gmp(i, j) < A_gmp(i, j)+threshold
+        }
+    }
+}
+
+
+// check postconditions
+TEST(TestQRDecomposition, TestPostconditionsNotSmallMatrix)
+{
+    // create a random size small matrix
+    int m;
+    int n;
+    do {
+        srand(time(NULL));
+        m = (rand() % 18) + 2;    // [2, ... ,20]
+        n = (rand() % 18) + 2;    // [2, ... ,20]
+    } while (m >= n);
+    hdnum::DenseMatrix<double> Q(m, n);
+
+    // fill it with random elements
+    for (int i=0; i < Q.rowsize(); i++) {
+        for (int j=0; j < Q.colsize(); j++) {
+            int x = (rand() % 200) - 100;    // [-100, ... ,100]
+            Q(i, j) = x;
+        }
+    }
+
+    // save it before overwritting
+    hdnum::DenseMatrix<double> A(Q);
+
+    // run qr decomposition and save R
+    hdnum::DenseMatrix<double> R(hdnum::qr_decomposition_gram_schmidt(Q));
+
+    // declare a threshold for the asserts
+    double threshold = 0.00000001;
+
+    // check Q ∈ K^(mxn)
+    ASSERT_EQ(Q.colsize(), A.colsize());
+    ASSERT_EQ(Q.rowsize(), A.rowsize());
+
+    // check that norm(qi) = 1
+    for (int i=0; i < Q.colsize(); i++) {
+        double norm(0.0);
+
+        for (int j=0; j < Q.rowsize(); j++) {
+            norm += Q(j, i) * Q(j, i);
+        }
+        norm = sqrt(norm);
+        ASSERT_NEAR(norm, 1.0, threshold);
+    }
+
+    // check that Q^T * Q = I
+    hdnum::DenseMatrix<double> I(Q.transpose()*Q);
+    for (int i=0; i < I.rowsize(); i++) {
+        for (int j=0; j < I.colsize(); j++) {
+            // main diagonal
+            if (j == i) {
+                ASSERT_NEAR(I(i, j), 1.0, threshold);
+                continue;
+            }
+            // other elements
+            ASSERT_NEAR(I(i, j), 0.0, threshold);
+        }
+    }
+
+    // R ∈ K^(nxn)
+    ASSERT_EQ(R.rowsize(), A.colsize());
+    ASSERT_EQ(R.colsize(), A.colsize());
+
+    // R is an upper triangular matrix
+    for (int i=0; i < R.colsize(); i++) {
+        for (int j=i+1; j < R.rowsize(); j++) {
+            ASSERT_NEAR(R(j, i), 0.0, threshold);
+        }
+    }
+
+    // A = Q*R
+    hdnum::DenseMatrix<double> QR(Q*R);
+    ASSERT_EQ(QR.rowsize(), A.rowsize());
+    ASSERT_EQ(QR.colsize(), A.colsize());
+    for (int i=0; i < QR.rowsize(); i++) {
+        for (int j=0; j < QR.colsize(); j++) {
+            ASSERT_NEAR(QR(i, j), A(i, j), threshold);
+        }
+    }
+
+    // now repeat with GNU multiple precision types
+    mpf_set_default_prec(2048);
+
+    // declare a new more precise threshold
+    mpf_class threshold_gmp = 0.00000000000000000001;
+
+    // create A, Q and R with gmp datatypes
+    hdnum::DenseMatrix<mpf_class> A_gmp(m, n);
+    for (int r=0; r < A_gmp.rowsize(); r++) {
+        for (int c=0; c < A_gmp.colsize(); c++) {
+            A_gmp(r, c) = A(r, c);
+        }
+    }
+
+    hdnum::DenseMatrix<mpf_class> Q_gmp(A_gmp);
+    hdnum::DenseMatrix<mpf_class> R_gmp(hdnum::qr_decomposition_gram_schmidt(Q_gmp));
+
+    // check that norm(qi) = 1
+    for (int i=0; i < Q_gmp.colsize(); i++) {
+        mpf_class norm = 0;
+        mpf_class expected_norm = 1;
+
+        for (int j=0; j < Q_gmp.rowsize(); j++) {
+            norm += Q_gmp(j, i) * Q_gmp(j, i);
+        }
+        norm = sqrt(norm);
+        ASSERT_TRUE( (norm > expected_norm-threshold_gmp) && (norm < expected_norm+threshold_gmp));     // 1-threshold < norm < 1+threshold
+    }
+
+    // check that Q_gmp^T * Q_gmp = I
+    hdnum::DenseMatrix<mpf_class> I_gmp(Q_gmp.transpose()*Q_gmp);
+    for (int i=0; i < I_gmp.rowsize(); i++) {
+        for (int j=0; j < I_gmp.colsize(); j++) {
+            // main diagonal
+            if (j == i) {
+                ASSERT_TRUE((I_gmp(i, i) > 1-threshold_gmp) && (I_gmp(i, i) < 1+threshold_gmp));         // 1-threshold < I(i, i) < 1+threshold
+                continue;
+            }
+            // other elements
+            ASSERT_TRUE((I_gmp(i, j) > 0-threshold_gmp) && (I_gmp(i, j) < 0+threshold_gmp));             // 0-threshold < I(i, j) < 0+threshold
+        }
+    }
+
+    // R_gmp ∈ K^(nxn)
+    ASSERT_EQ(R_gmp.rowsize(), A_gmp.colsize());
+    ASSERT_EQ(R_gmp.colsize(), A_gmp.colsize());
+
+    // R_gmp is an upper triangular matrix
+    for (int i=0; i < R_gmp.colsize(); i++) {
+        for (int j=i+1; j < R_gmp.rowsize(); j++) {
+            ASSERT_TRUE((R_gmp(j, i) > 0-threshold_gmp) && (R_gmp(j, i) < 0+threshold_gmp));             // 0-threshold < I(i, j) < 0+threshold
+        }
+    }
+
+    // A_gmp = Q_gmp*R_gmp
+    hdnum::DenseMatrix<mpf_class> QR_gmp(Q_gmp*R_gmp);
+    ASSERT_EQ(QR_gmp.rowsize(), A_gmp.rowsize());
+    ASSERT_EQ(QR_gmp.colsize(), A_gmp.colsize());
+    for (int i=0; i < QR_gmp.rowsize(); i++) {
+        for (int j=0; j < QR_gmp.colsize(); j++) {
+            ASSERT_TRUE((QR_gmp(i, j) > A_gmp(i, j)-threshold_gmp) && (QR_gmp(i, j) < A_gmp(i, j)+threshold_gmp));     // A_gmp(i, j)-threshold < QR_gmp(i, j) < A_gmp(i, j)+threshold
+        }
+    }
 }
 
 // check postconditions
