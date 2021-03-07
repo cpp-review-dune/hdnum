0830edc8efc33ba785011a22cdf3ee5c51c0f5af
raphael.vogt <cx238@stud.uni-heidelberg.de>
2021-03-07 16:07:30 +0100

    new tests for read matrix market file into a densematrix

--- test/read_matrix_from_file.cc
+++ test/read_matrix_from_file.cc
@@ -6,57 +6,95 @@
 #include "gtest/gtest.h"
 
 namespace {
-// In this example, we test the operators for the SparseMatrix class.
+// In this example, we test the read Matrix function from densematrix and sparsematrix
 
 using hdnum::SparseMatrix;
+using hdnum::DenseMatrix;
 
-class TestReadSparseMatrixFromFile : public ::testing::Test {
+class TestReadMatrixFromFile : public ::testing::Test {
 public:
-    const SparseMatrix<double> A;
+    const SparseMatrix<double> A_sparse;
     const std::string filename = "../example_matrix_market.mtx";
 
-    const SparseMatrix<double> A_empty;
+    const DenseMatrix<double> A_dense;
+
+    const SparseMatrix<double> A_sparse_empty;
     const std::string filename_empty = "../example_empty_matrix_market.mtx";
+
+    const DenseMatrix<double> A_dense_empty;
 
     using size_type = typename SparseMatrix<double>::size_type;
 };
 
-TEST_F(TestReadSparseMatrixFromFile, ReadInMatrixFromEmptyFile) {
-    SparseMatrix<double> B_empty {};
-    hdnum::readMatrixFromFile(this->filename_empty, B_empty);
+TEST_F(TestReadMatrixFromFile, ReadInMatrixFromEmptyFile) {
+    SparseMatrix<double> B_sparse_empty {};
+    hdnum::readMatrixFromFile(this->filename_empty, B_sparse_empty);
 
     // check sparse matrix is empty
-    ASSERT_EQ(B_empty.rowsize(), 0);
-    ASSERT_EQ(B_empty.colsize(), 0);
+    ASSERT_EQ(B_sparse_empty.rowsize(), 0);
+    ASSERT_EQ(B_sparse_empty.colsize(), 0);
+
+    DenseMatrix<double> B_dense_empty {};
+    hdnum::readMatrixFromFileMatrixMarket(this->filename_empty, B_dense_empty);
+
+    // check sparse matrix is empty
+    ASSERT_EQ(B_dense_empty.rowsize(), 0);
+    ASSERT_EQ(B_dense_empty.colsize(), 0);
 }
 
-TEST_F(TestReadSparseMatrixFromFile, ReadInMatrixFromValidFile) {
-    SparseMatrix<double> B {};
-    hdnum::readMatrixFromFile(this->filename, B);
+TEST_F(TestReadMatrixFromFile, ReadInMatrixFromValidFile) {
+    SparseMatrix<double> B_sparse {};
+    hdnum::readMatrixFromFile(this->filename, B_sparse);
 
-    ASSERT_EQ(B.rowsize(), 5);
-    ASSERT_EQ(B.colsize(), 5);
+    ASSERT_EQ(B_sparse.rowsize(), 5);
+    ASSERT_EQ(B_sparse.colsize(), 5);
 
     std::vector<std::pair<int, int>> nonzeros = {
         {0, 0}, {4, 0}, {1, 1}, {2, 2}, {3, 3}};
 
     // check nonzero elements
-    ASSERT_EQ(B(0, 0), 1474.779);
-    ASSERT_EQ(B(4, 0), -9.017133);
-    ASSERT_EQ(B(1, 1), 9.136654);
-    ASSERT_EQ(B(2, 2), 69.61468);
-    ASSERT_EQ(B(3, 3), 68.60106);
+    ASSERT_EQ(B_sparse(0, 0), 1474.779);
+    ASSERT_EQ(B_sparse(4, 0), -9.017133);
+    ASSERT_EQ(B_sparse(1, 1), 9.136654);
+    ASSERT_EQ(B_sparse(2, 2), 69.61468);
+    ASSERT_EQ(B_sparse(3, 3), 68.60106);
 
     // check zero elements
-    for (int r = 0; r < B.rowsize(); r++) {
-        for (int c = 0; c < B.colsize(); c++) {
+    for (int r = 0; r < B_sparse.rowsize(); r++) {
+        for (int c = 0; c < B_sparse.colsize(); c++) {
             // nonzero
             if (std::find(nonzeros.begin(), nonzeros.end(),
                           std::make_pair(r, c)) != nonzeros.end()) {
                 continue;
             }
             // zero
-            ASSERT_EQ(B(r, c), 0);
+            ASSERT_EQ(B_sparse(r, c), 0);
+        }
+    }
+
+    DenseMatrix<double> B_dense {};
+    hdnum::readMatrixFromFileMatrixMarket(this->filename, B_dense);
+
+    ASSERT_EQ(B_dense.rowsize(), 5);
+    ASSERT_EQ(B_dense.colsize(), 5);
+
+    // check nonzero elements
+    ASSERT_EQ(B_dense(0, 0), 1474.779);
+    ASSERT_EQ(B_dense(4, 0), -9.017133);
+    ASSERT_EQ(B_dense(1, 1), 9.136654);
+    ASSERT_EQ(B_dense(2, 2), 69.61468);
+    ASSERT_EQ(B_dense(3, 3), 68.60106);
+
+    // check zero elements
+    for (int r = 0; r < B_dense.rowsize(); r++) {
+        for (int c = 0; c < B_dense.colsize(); c++) {
+            // nonzero
+            if (std::find(nonzeros.begin(), nonzeros.end(),
+                          std::make_pair(r, c)) != nonzeros.end()) {
+                continue;
+            }
+            // zero
+            ASSERT_EQ(B_dense(r, c), 0);
         }
     }
 }
