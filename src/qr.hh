caa349be1ca8f2e85b9439846b2a7b25c84bcce7
raphael.vogt <cx238@stud.uni-heidelberg.de>
2021-03-08 20:17:41 +0100

    now also works for matrices n > m

--- src/qr.hh
+++ src/qr.hh
@@ -79,56 +79,120 @@ DenseMatrix<T> modified_gram_schmidt(const DenseMatrix<T>& A) {
 //! computes qr decomposition using modified Gram-Schmidt
 template <class T>
 DenseMatrix<T> qr_decomposition_gram_schmidt(DenseMatrix<T>& Q) {
-    // check preconditions
-    // save matrix A, before it's replaced with Q
-    DenseMatrix<T> A(Q);
+    // small and square matrices
+    if (Q.rowsize() >= Q.colsize()) {
+        // save matrix A, before it's replaced with Q
+        DenseMatrix<T> A(Q);
 
-    // create matrix R
-    DenseMatrix<T> R(Q.colsize(), Q.colsize());
+        // create matrix R
+        DenseMatrix<T> R(Q.colsize(), Q.colsize());
 
-    for (int k = 0; k < Q.colsize(); k++) {
-        // modify all later columns with column k
-        for (int j = k + 1; j < Q.colsize(); j++) {
-            // compute factor
-            T sum_nom(0.0);
-            T sum_denom(0.0);
-            for (int i = 0; i < Q.rowsize(); i++) {
-                sum_nom += Q(i, j) * Q(i, k);
-                sum_denom += Q(i, k) * Q(i, k);
+        // start orthogonalizing
+        for (int k = 0; k < Q.colsize(); k++) {
+            // modify all later columns with column k
+            for (int j = k + 1; j < Q.colsize(); j++) {
+                // compute factor
+                T sum_nom(0.0);
+                T sum_denom(0.0);
+                for (int i = 0; i < Q.rowsize(); i++) {
+                    sum_nom += Q(i, j) * Q(i, k);
+                    sum_denom += Q(i, k) * Q(i, k);
+                }
+
+                T alpha = sum_nom / sum_denom;
+                for (int i = 0; i < Q.rowsize(); i++) Q(i, j) -= alpha * Q(i, k);
             }
-
-            T alpha = sum_nom / sum_denom;
-            for (int i = 0; i < Q.rowsize(); i++) Q(i, j) -= alpha * Q(i, k);
         }
-    }
 
-    // add values to R, except main diagonal
-    for (int i = 1; i < R.colsize(); i++) {
-        for (int j = 0; j < i; j++) {
-            T sum_nom(0.0);
-            T sum_l2nom(0.0);
-            for (int k = 0; k < Q.rowsize(); k++) {
-                sum_nom += A(k, i) * Q(k, j);
-                sum_l2nom += Q(k, j) * Q(k, j);
+        // add values to R, except main diagonal
+        for (int i = 1; i < R.colsize(); i++) {
+            for (int j = 0; j < i; j++) {
+                T sum_nom(0.0);
+                T sum_l2nom(0.0);
+                for (int k = 0; k < Q.rowsize(); k++) {
+                    sum_nom += A(k, i) * Q(k, j);
+                    sum_l2nom += Q(k, j) * Q(k, j);
+                }
+                sum_l2nom = sqrt(sum_l2nom);
+                // add element
+                R(j, i) = sum_nom / sum_l2nom;
             }
-            sum_l2nom = sqrt(sum_l2nom);
-            // add element
-            R(j, i) = sum_nom / sum_l2nom;
         }
-    }
 
-    for (int j = 0; j < Q.colsize(); j++) {
-        // compute norm of column j
-        T sum(0.0);
-        for (int i = 0; i < Q.rowsize(); i++) sum += Q(i, j) * Q(i, j);
-        sum = sqrt(sum);
-        // add main diagonal to R
-        R(j, j) = sum;
-        // scale Q
-        for (int i = 0; i < Q.rowsize(); i++) Q(i, j) = Q(i, j) / sum;
+        // add missing values and scale
+        for (int j = 0; j < Q.colsize(); j++) {
+            // compute norm of column j
+            T sum(0.0);
+            for (int i = 0; i < Q.rowsize(); i++) sum += Q(i, j) * Q(i, j);
+            sum = sqrt(sum);
+            // add main diagonal to R
+            R(j, j) = sum;
+            // scale Q
+            for (int i = 0; i < Q.rowsize(); i++) Q(i, j) = Q(i, j) / sum;
+        }
+        return R;
     }
+    
+    // matrices n > m
+    else {
+        // create Q matrix with the right dimensions
+        DenseMatrix<T> Q_temp(Q.rowsize(), Q.rowsize());
+        // copy the elements
+        for (int i = 0; i < Q_temp.rowsize(); i++) {
+            for (int j = 0; j < Q_temp.colsize(); j++) {
+                Q_temp(i, j) = Q(i, j);
+            }
+        }
 
-    return R;
+        // create matrix R
+        DenseMatrix<T> R(Q.rowsize(), Q.colsize());
+
+        // start orthogonalizing
+        for (int k = 0; k < Q_temp.colsize(); k++) {
+            // modify all later columns with column k
+            for (int j = k + 1; j < Q_temp.colsize(); j++) {
+                // compute factor
+                T sum_nom(0.0);
+                T sum_denom(0.0);
+                for (int i = 0; i < Q.rowsize(); i++) {
+                    sum_nom += Q_temp(i, j) * Q_temp(i, k);
+                    sum_denom += Q_temp(i, k) * Q_temp(i, k);
+                }
+
+                T alpha = sum_nom / sum_denom;
+                for (int i = 0; i < Q_temp.rowsize(); i++) Q_temp(i, j) -= alpha * Q_temp(i, k);
+            }
+        }
+
+        // add values to R, except main diagonal
+        for (int i = 1; i < R.colsize(); i++) {
+            for (int j = 0; j < i && j < R.rowsize(); j++) {
+                T sum_nom(0.0);
+                T sum_l2nom(0.0);
+                for (int k = 0; k < Q_temp.rowsize(); k++) {
+                    sum_nom += Q(k, i) * Q_temp(k, j);
+                    sum_l2nom += Q_temp(k, j) * Q_temp(k, j);
+                }
+                sum_l2nom = sqrt(sum_l2nom);
+                // add element
+                R(j, i) = sum_nom / sum_l2nom;
+            }
+        }
+
+        // add missing values and scale
+        for (int j = 0; j < Q_temp.colsize(); j++) {
+            // compute norm of column j
+            T sum(0.0);
+            for (int i = 0; i < Q_temp.rowsize(); i++) sum += Q_temp(i, j) * Q_temp(i, j);
+            sum = sqrt(sum);
+            // add main diagonal to R
+            if (j < R.rowsize()) R(j, j) = sum;
+            // scale Q
+            for (int i = 0; i < Q_temp.rowsize(); i++) Q_temp(i, j) = Q_temp(i, j) / sum;
+        }
+        Q = Q_temp;
+        return R;
+    }
 }
 
 //! computes qr decomposition using modified Gram-Schmidt and pivoting
