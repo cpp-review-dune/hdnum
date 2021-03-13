4f1db7c115830e3114eb74277c4d802ca9f191cf
raphael.vogt <cx238@stud.uni-heidelberg.de>
2021-03-13 15:38:05 +0100

    works now for all types of matrices

--- src/qr.hh
+++ src/qr.hh
@@ -76,167 +76,17 @@ DenseMatrix<T> modified_gram_schmidt(const DenseMatrix<T>& A) {
     return Q;
 }
 
-//! computes qr decomposition using modified Gram-Schmidt
+//! computes qr decomposition using modified Gram-Schmidt - works only with small and aquare matrices
 template <class T>
-DenseMatrix<T> qr_decomposition_gram_schmidt(DenseMatrix<T>& Q) {
-    // small and square matrices
-    if (Q.rowsize() >= Q.colsize()) {
-        // save matrix A, before it's replaced with Q
-        DenseMatrix<T> A(Q);
-
-        // create matrix R
-        DenseMatrix<T> R(Q.colsize(), Q.colsize());
-
-        // start orthogonalizing
-        for (int k = 0; k < Q.colsize(); k++) {
-            // modify all later columns with column k
-            for (int j = k + 1; j < Q.colsize(); j++) {
-                // compute factor
-                T sum_nom(0.0);
-                T sum_denom(0.0);
-                for (int i = 0; i < Q.rowsize(); i++) {
-                    sum_nom += Q(i, j) * Q(i, k);
-                    sum_denom += Q(i, k) * Q(i, k);
-                }
-
-                T alpha = sum_nom / sum_denom;
-                for (int i = 0; i < Q.rowsize(); i++) Q(i, j) -= alpha * Q(i, k);
-            }
-        }
-
-        // add values to R, except main diagonal
-        for (int i = 1; i < R.colsize(); i++) {
-            for (int j = 0; j < i; j++) {
-                T sum_nom(0.0);
-                T sum_l2nom(0.0);
-                for (int k = 0; k < Q.rowsize(); k++) {
-                    sum_nom += A(k, i) * Q(k, j);
-                    sum_l2nom += Q(k, j) * Q(k, j);
-                }
-                sum_l2nom = sqrt(sum_l2nom);
-                // add element
-                R(j, i) = sum_nom / sum_l2nom;
-            }
-        }
-
-        // add missing values and scale
-        for (int j = 0; j < Q.colsize(); j++) {
-            // compute norm of column j
-            T sum(0.0);
-            for (int i = 0; i < Q.rowsize(); i++) sum += Q(i, j) * Q(i, j);
-            sum = sqrt(sum);
-            // add main diagonal to R
-            R(j, j) = sum;
-            // scale Q
-            for (int i = 0; i < Q.rowsize(); i++) Q(i, j) = Q(i, j) / sum;
-        }
-        return R;
-    }
-    
-    // matrices n > m
-    else {
-        // create Q matrix with the right dimensions
-        DenseMatrix<T> Q_temp(Q.rowsize(), Q.rowsize());
-        // copy the elements
-        for (int i = 0; i < Q_temp.rowsize(); i++) {
-            for (int j = 0; j < Q_temp.colsize(); j++) {
-                Q_temp(i, j) = Q(i, j);
-            }
-        }
-
-        // create matrix R
-        DenseMatrix<T> R(Q.rowsize(), Q.colsize());
-
-        // start orthogonalizing
-        for (int k = 0; k < Q_temp.colsize(); k++) {
-            // modify all later columns with column k
-            for (int j = k + 1; j < Q_temp.colsize(); j++) {
-                // compute factor
-                T sum_nom(0.0);
-                T sum_denom(0.0);
-                for (int i = 0; i < Q.rowsize(); i++) {
-                    sum_nom += Q_temp(i, j) * Q_temp(i, k);
-                    sum_denom += Q_temp(i, k) * Q_temp(i, k);
-                }
-
-                T alpha = sum_nom / sum_denom;
-                for (int i = 0; i < Q_temp.rowsize(); i++) Q_temp(i, j) -= alpha * Q_temp(i, k);
-            }
-        }
-
-        // add values to R, except main diagonal
-        for (int i = 1; i < R.colsize(); i++) {
-            for (int j = 0; j < i && j < R.rowsize(); j++) {
-                T sum_nom(0.0);
-                T sum_l2nom(0.0);
-                for (int k = 0; k < Q_temp.rowsize(); k++) {
-                    sum_nom += Q(k, i) * Q_temp(k, j);
-                    sum_l2nom += Q_temp(k, j) * Q_temp(k, j);
-                }
-                sum_l2nom = sqrt(sum_l2nom);
-                // add element
-                R(j, i) = sum_nom / sum_l2nom;
-            }
-        }
-
-        // add missing values and scale
-        for (int j = 0; j < Q_temp.colsize(); j++) {
-            // compute norm of column j
-            T sum(0.0);
-            for (int i = 0; i < Q_temp.rowsize(); i++) sum += Q_temp(i, j) * Q_temp(i, j);
-            sum = sqrt(sum);
-            // add main diagonal to R
-            if (j < R.rowsize()) R(j, j) = sum;
-            // scale Q
-            for (int i = 0; i < Q_temp.rowsize(); i++) Q_temp(i, j) = Q_temp(i, j) / sum;
-        }
-        Q = Q_temp;
-        return R;
-    }
-}
-
-//! computes qr decomposition using modified Gram-Schmidt and pivoting
-template <class T>
-DenseMatrix<T> qr_decomposition_gram_schmidt_pivoting(DenseMatrix<T>& Q) {
-    // check preconditions
+DenseMatrix<T> qr_gram_schmidt_simple(DenseMatrix<T>& Q) {
     // save matrix A, before it's replaced with Q
     DenseMatrix<T> A(Q);
 
     // create matrix R
     DenseMatrix<T> R(Q.colsize(), Q.colsize());
 
-    // save column with highest norm
-    std::pair<int, T> highest_norm(0, 0);
-
-    // threshold for rank reveal
-    double threshold = 0.00000000001;
-
+    // start orthogonalizing
     for (int k = 0; k < Q.colsize(); k++) {
-        highest_norm.first = 0;
-        highest_norm.second = 0;
-
-        // find column with highest norm
-        for (int c = k; c < Q.colsize(); c++) {
-            T norm(0.0);
-            for (int r = 0; r < Q.rowsize(); r++) {
-                norm += Q(r, c) * Q(r, c);
-            }
-            norm = sqrt(norm);
-            if (norm > highest_norm.second) {
-                highest_norm.first = c;
-                highest_norm.second = norm;
-            }
-        }
-
-        // swap columns if necessary
-        if (highest_norm.first != k) {
-            for (int r = 0; r < Q.rowsize(); r++) {
-                T temp_Q = Q(r, k);
-                Q(r, k) = Q(r, highest_norm.first);
-                Q(r, highest_norm.first) = temp_Q;
-            }
-        }
-
         // modify all later columns with column k
         for (int j = k + 1; j < Q.colsize(); j++) {
             // compute factor
@@ -249,11 +99,6 @@ DenseMatrix<T> qr_decomposition_gram_schmidt_pivoting(DenseMatrix<T>& Q) {
 
             T alpha = sum_nom / sum_denom;
             for (int i = 0; i < Q.rowsize(); i++) Q(i, j) -= alpha * Q(i, k);
-        }
-
-        // norm < given threshold -> A hasn't full rank
-        if (highest_norm.second < threshold) {
-            HDNUM_ERROR("The Matrix has not full rank!");
         }
     }
 
@@ -272,6 +117,7 @@ DenseMatrix<T> qr_decomposition_gram_schmidt_pivoting(DenseMatrix<T>& Q) {
         }
     }
 
+    // add missing values and scale
     for (int j = 0; j < Q.colsize(); j++) {
         // compute norm of column j
         T sum(0.0);
@@ -282,8 +128,162 @@ DenseMatrix<T> qr_decomposition_gram_schmidt_pivoting(DenseMatrix<T>& Q) {
         // scale Q
         for (int i = 0; i < Q.rowsize(); i++) Q(i, j) = Q(i, j) / sum;
     }
-
     return R;
+}
+
+//! computes qr decomposition using modified Gram-Schmidt - works only with small and aquare matrices
+template <class T>
+DenseMatrix<T> qr_gram_schmidt(DenseMatrix<T>& Q) {
+    // create matrix R
+    DenseMatrix<T> R(Q.colsize(), Q.colsize());
+
+    // start orthogonalizing
+    for (int k = 0; k < Q.colsize(); k++) {
+        // compute norm of column k
+        T sum_denom(0.0);
+        for (int i = 0; i < Q.rowsize(); i++) {
+            sum_denom += Q(i, k) * Q(i, k);
+        }
+
+        // fill the main diagonal of R with elements
+        sum_denom = sqrt(sum_denom);
+        R(k, k) = sum_denom;
+
+        // scale column k to the main diagonal
+        for (int i = 0; i < Q.rowsize(); i++) {
+            Q(i, k) /= R(k, k);
+        }
+
+        // modify all later columns with column k
+        for (int j = k + 1; j < Q.colsize(); j++) {
+            // compute norm of column j
+            T sum_nom(0.0);
+            for (int i = 0; i < Q.rowsize(); i++) {
+                sum_nom += Q(i, k) * Q(i, j);
+            }
+            // insert missing elements to R
+            R(k, j) = sum_nom;
+
+            // orthogonalize column j
+            for (int i = 0; i < Q.rowsize(); i++) {
+                Q(i, j) -= Q(i, k) * R(k, j);
+            }
+        }
+    }
+    return R;
+}
+
+//! computes qr decomposition using modified Gram-Schmidt and pivoting - works with all types of matrices
+template <class T>
+DenseMatrix<T> qr_gram_schmidt_pivoting(DenseMatrix<T>& Q, Vector<int>& p, int& rank, double threshold=0.00000000001) {
+    // check if permutation vector has the right size
+    if (p.size() != Q.colsize()) {
+        HDNUM_ERROR("Permutation Vector incompatible with Matrix!");
+    }
+
+    // initialize permutation vector
+    for (int i = 0; i < p.size(); i++) {
+        p[i] = i;
+    }
+
+    // initialize rank
+    rank = 0;
+
+    // create matrix R
+    DenseMatrix<T> R(Q.colsize(), Q.colsize());
+
+    // start orthogonalizing
+    for (int k = 0; k < Q.colsize(); k++) {
+        // find column with highest norm
+        // compute norm of column k
+        T norm_k(0.0);
+        for (int r = 0; r < Q.rowsize(); r++) {
+                norm_k += Q(r, k) * Q(r, k);
+            }
+        norm_k = sqrt(norm_k);
+
+        // compare norm of column k to the following column norms
+        for (int c = k+1; c < Q.colsize(); c++) {
+            T norm(0.0);
+            for (int r = 0; r < Q.rowsize(); r++) {
+                norm += Q(r, c) * Q(r, c);
+            }
+            norm = sqrt(norm);
+            // store permutation
+            if (norm > norm_k) {
+                p[k] = c;
+            }
+        }
+
+        // swap columns if necessary
+        if (p[k] > k) {
+            for (int r = 0; r < Q.rowsize(); r++) {
+                T temp_Q = Q(r, k);
+                Q(r, k) = Q(r, p[k]);
+                Q(r, p[k]) = temp_Q;
+            }
+
+            // compute norm of the new column k
+            norm_k = 0;
+            for (int i = 0; i < Q.rowsize(); i++) {
+                norm_k += Q(i, k) * Q(i, k);
+            }
+            norm_k = sqrt(norm_k);
+        }
+
+        // if norm of column k > threshold -> column k is linear independent
+        if (norm_k > threshold) {
+            rank++;
+        } else {
+            break;
+        }
+
+        // fill the main diagonal of R with elements
+        R(k, k) = norm_k;
+
+        // scale column k to the main diagonal
+        for (int i = 0; i < Q.rowsize(); i++) {
+            Q(i, k) /= R(k, k);
+        }
+
+        // modify all later columns with column k
+        for (int j = k + 1; j < Q.colsize(); j++) {
+            // compute norm of column j
+            T sum_nom(0.0);
+            for (int i = 0; i < Q.rowsize(); i++) {
+                sum_nom += Q(i, k) * Q(i, j);
+            }
+            // insert missing elements to R
+            R(k, j) = sum_nom;
+
+            // orthogonalize column j
+            for (int i = 0; i < Q.rowsize(); i++) {
+                Q(i, j) -= Q(i, k) * R(k, j);
+            }
+        }
+    }
+    return R;
+}
+
+//! applies a permutation vector to a matrix
+template<typename T>
+void apply_permutation(DenseMatrix<T>& A, const Vector<int>& p) {
+    // check if permutation vector has the right size
+    if (p.size() != A.colsize()) {
+        HDNUM_ERROR("Permutation Vector incompatible with Matrix!");
+    }
+
+    // permutate the columns
+    for (int k = 0; k < p.size(); k++) {
+        if (p[k] != k) {
+            // swap column
+            for (int j=0; j < A.rowsize(); j++) {
+                T temp_A = A(j, k);
+                A(j, k) = A(j, p[k]);
+                A(j, p[k]) = temp_A;
+            }
+        }
+    }
 }
 }  // namespace hdnum
 
