a110b886bf68988d2ddc615847ea133b7d1d2a0e
Ahmad Fadl <abohmaid@windowslive.com>
2021-03-16 10:32:21 +0100

    foramtierung

--- test/test_qrhousholder.cc
+++ test/test_qrhousholder.cc
@@ -3,7 +3,7 @@
 #include "../hdnum.hh"
 #include "gtest/gtest.h"
 
-//test R and Vi vectors for quadratic matrix
+// test R and Vi vectors for quadratic matrix
 
 TEST(TestQRhousholder, TestPostconditionsqrhousholderRandVi) {
     // sample matrix where we know the expected results
@@ -30,30 +30,31 @@ TEST(TestQRhousholder, TestPostconditionsqrhousholderRandVi) {
     ASSERT_NEAR(v[2], -4.0, threshold);
 }
 
-//test R and Vi vectors for quadratic matrix
+// test R and Vi vectors for quadratic matrix
 
-TEST(TestQRhousholder, TestQRhousholder_Test_Postconditions_qrhousholder_quadratisch){
-    //sample matrix where we know the expected results
-    hdnum::DenseMatrix<double> A={{1, -1, 4},{1 ,4, -2},{1 ,4, 2}};
-    hdnum::Vector<double>V(A.colsize(),0) ;
+TEST(TestQRhousholder,
+     TestQRhousholder_Test_Postconditions_qrhousholder_quadratisch) {
+    // sample matrix where we know the expected results
+    hdnum::DenseMatrix<double> A = {{1, -1, 4}, {1, 4, -2}, {1, 4, 2}};
+    hdnum::Vector<double> V(A.colsize(), 0);
     double threshold = 0.0001;
-    //test if R and V_{i} correct after calling the function
-    qrhousholder(A,V);
-    ASSERT_NEAR(A(0,0), 2.7321, threshold);
-    ASSERT_NEAR(A(1,0), 1.0, threshold);
-    ASSERT_NEAR(A(2,0), 1.0, threshold);
-    ASSERT_NEAR(A(0,1), -4.0415, threshold);
-    ASSERT_NEAR(A(1,1), 6.9692, threshold);
-    ASSERT_NEAR(A(2,1), 2.8868, threshold);
-    ASSERT_NEAR(A(0,2), -2.3094, threshold);
-    ASSERT_NEAR(A(1,2), 3.266, threshold);
-    ASSERT_NEAR(A(2,2), 5.6569, threshold);
+    // test if R and V_{i} correct after calling the function
+    qrhousholder(A, V);
+    ASSERT_NEAR(A(0, 0), 2.7321, threshold);
+    ASSERT_NEAR(A(1, 0), 1.0, threshold);
+    ASSERT_NEAR(A(2, 0), 1.0, threshold);
+    ASSERT_NEAR(A(0, 1), -4.0415, threshold);
+    ASSERT_NEAR(A(1, 1), 6.9692, threshold);
+    ASSERT_NEAR(A(2, 1), 2.8868, threshold);
+    ASSERT_NEAR(A(0, 2), -2.3094, threshold);
+    ASSERT_NEAR(A(1, 2), 3.266, threshold);
+    ASSERT_NEAR(A(2, 2), 5.6569, threshold);
     ASSERT_NEAR(V[0], -1.7321, threshold);
     ASSERT_NEAR(V[1], -4.0825, threshold);
     ASSERT_NEAR(V[2], -2.8284, threshold);
 }
 
-//test Q = Qt for a random matrix
+// test Q = Qt for a random matrix
 TEST(TestQRhousholder, TestPostconditionsqrhousholderexplizitQ) {
     // create a random size small matrix
     int m;
@@ -75,15 +76,15 @@ TEST(TestQRhousholder, TestPostconditionsqrhousholderexplizitQ) {
     }
     hdnum::DenseMatrix<double> I(m, m);
     hdnum::identity(I);
-    hdnum::DenseMatrix<double>  q = hdnum::qrhousholderexplizitQ(A, v, false);
-    hdnum::DenseMatrix<double>  res = q * q.transpose();
-    for (size_t i=0;i<m;i++){
-        for (size_t j =0;j<m ;j++){
-            if(j==i){
-                ASSERT_NEAR(res(i,j),1.0,threshold);
+    hdnum::DenseMatrix<double> q = hdnum::qrhousholderexplizitQ(A, v, false);
+    hdnum::DenseMatrix<double> res = q * q.transpose();
+    for (size_t i = 0; i < m; i++) {
+        for (size_t j = 0; j < m; j++) {
+            if (j == i) {
+                ASSERT_NEAR(res(i, j), 1.0, threshold);
             }
-            if(j!=i){
-                ASSERT_NEAR(res(i,j),0.0,threshold);
+            if (j != i) {
+                ASSERT_NEAR(res(i, j), 0.0, threshold);
             }
         }
     }
@@ -99,7 +100,7 @@ TEST(TestQRhousholder, TestPostconditionsqrhousholderexplizitQ_results) {
         srand(time(NULL));
         m = (rand() % 4) + 2;  // [2, ... ,20]
         n = (rand() % 4) + 2;  // [2, ... ,20]
-    } while (m<= n);
+    } while (m <= n);
     hdnum::DenseMatrix<double> A(m, n);
     hdnum::Vector<double> v(A.colsize(), 0);
     // fill it with random elements
@@ -112,31 +113,28 @@ TEST(TestQRhousholder, TestPostconditionsqrhousholderexplizitQ_results) {
     hdnum::DenseMatrix<double> orginal(A);
     hdnum::DenseMatrix<double> I(m, m);
     hdnum::identity(I);
-    hdnum::DenseMatrix<double>  q = hdnum::qrhousholderexplizitQ(A, v, false);
-    
-    
+    hdnum::DenseMatrix<double> q = hdnum::qrhousholderexplizitQ(A, v, false);
 
-    for (size_t i =0; i<m;i++){
-        for(size_t j=0;j<n;j++){
-            if(i==j){
-                A(i,j)=v[i];
+    for (size_t i = 0; i < m; i++) {
+        for (size_t j = 0; j < n; j++) {
+            if (i == j) {
+                A(i, j) = v[i];
                 continue;
             }
-            if(i>j){
-                A(i,j)=0;
+            if (i > j) {
+                A(i, j) = 0;
             }
         }
     }
-    hdnum::DenseMatrix<double> res (q * A);
-    for (size_t i=0;i<m;i++){
-        for (size_t j =0;j<m ;j++){
-            
-            ASSERT_NEAR(res(i,j),orginal(i,j),threshold);
-            
+    hdnum::DenseMatrix<double> res(q * A);
+    for (size_t i = 0; i < m; i++) {
+        for (size_t j = 0; j < m; j++) {
+            ASSERT_NEAR(res(i, j), orginal(i, j), threshold);
         }
     }
 }
-TEST(TestQRhousholder, TestPostconditionsqrhousholderexplizitQ_results_quadrat) {
+TEST(TestQRhousholder,
+     TestPostconditionsqrhousholderexplizitQ_results_quadrat) {
     // create a random size small matrix
     int m;
     int n;
@@ -158,27 +156,23 @@ TEST(TestQRhousholder, TestPostconditionsqrhousholderexplizitQ_results_quadrat) 
     hdnum::DenseMatrix<double> orginal(A);
     hdnum::DenseMatrix<double> I(m, m);
     hdnum::identity(I);
-    hdnum::DenseMatrix<double>  q = hdnum::qrhousholderexplizitQ(A, v, false);
-    
-    
+    hdnum::DenseMatrix<double> q = hdnum::qrhousholderexplizitQ(A, v, false);
 
-    for (size_t i =0; i<m;i++){
-        for(size_t j=0;j<n;j++){
-            if(i==j){
-                A(i,j)=v[i];
+    for (size_t i = 0; i < m; i++) {
+        for (size_t j = 0; j < n; j++) {
+            if (i == j) {
+                A(i, j) = v[i];
                 continue;
             }
-            if(i>j){
-                A(i,j)=0;
+            if (i > j) {
+                A(i, j) = 0;
             }
         }
     }
-    hdnum::DenseMatrix<double> res (q * A);
-    for (size_t i=0;i<m;i++){
-        for (size_t j =0;j<m ;j++){
-            
-            ASSERT_NEAR(res(i,j),orginal(i,j),threshold);
-            
+    hdnum::DenseMatrix<double> res(q * A);
+    for (size_t i = 0; i < m; i++) {
+        for (size_t j = 0; j < m; j++) {
+            ASSERT_NEAR(res(i, j), orginal(i, j), threshold);
         }
     }
 }
