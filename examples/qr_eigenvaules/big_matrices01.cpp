2c300baae2f24f3b12f64a72073b089c3f5b4186
og272 <og272@stud.uni-heidelberg.de>
2023-12-08 19:01:17 +0100

    time calculation big matrices

--- examples/qr_eigenvaules/big_matrices01.cpp
+++ examples/qr_eigenvaules/big_matrices01.cpp
@@ -1,6 +1,8 @@
 
 #include <iostream>    
 #include "../../../hdnum/src/qr_eigenvalues.hh" //TODO change to cmake
+#include "../../../hdnum/src/timer.hh" //TODO change to cmake
+
 
 
 using namespace hdnum;
@@ -10,7 +12,7 @@ using Number = double;
 using Mat = DenseMatrix<Number>;
 
 Mat makeBigMatrix(int n){
-    std::srand(1);
+    std::srand(1); //seed can be changed
     Mat A= Mat(n, n, 1);
     for (int i=0; i<n; i++){
         for (int j=0; j<n; j++){
@@ -22,16 +24,16 @@ Mat makeBigMatrix(int n){
 
 int main(){
 
-    Mat A= makeBigMatrix(12); 
+    Mat A= makeBigMatrix(10); //TODO Error at 62
     A.scientific(false);
 
-    std::cout <<A << std::endl;
 
-    
     std::vector<Number> real;
     std::vector<Number> imag;
+    Timer t;
+    t.reset();
     eigenvalues_qr_algorithm_givens(A, real, imag);
-
+    std::cout << "time: " << t.elapsed() << "sec" << std::endl;
     for (int i = 0; i< real.size(); i++){
         std::cout << real[i]  << " + i*" << imag[i] << std::endl;
     }
